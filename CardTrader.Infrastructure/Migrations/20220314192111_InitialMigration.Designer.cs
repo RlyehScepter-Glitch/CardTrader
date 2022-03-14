4aec984561f7a032a3d3739770a01cc578dcf8e5
RlyehScepter <rlyehscepter@gmail.com>
2022-03-14 21:26:22 +0200

    Migrations moved out of Data folder/ Database up and running

--- CardTrader.Infrastructure/Migrations/20220314192111_InitialMigration.Designer.cs
+++ CardTrader.Infrastructure/Migrations/20220314192111_InitialMigration.Designer.cs
@@ -4,16 +4,18 @@ using CardTrader.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace CardTrader.Infrastructure.Data.Migrations
+namespace CardTrader.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220314192111_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
