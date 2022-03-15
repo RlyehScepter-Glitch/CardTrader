7db9a537044ff67728e9942602bb7362d5f35c3a
RlyehScepter <rlyehscepter@gmail.com>
2022-03-15 21:24:02 +0200

    Collection model made abstract

---
+++ CardTrader.Infrastructure/Data/Models/Collection.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardTrader.Infrastructure.Data.Models
+{
+    public abstract class Collection
+    {
+        [Key]
+        [StringLength(36)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        
+        //public List<Collection> BulkCards { get; set; }
+
+        [Required]
+        public User User { get; set; }
+        public string UserId { get; set; }
+
+        public ICollection<CardCollection> Cards { get; set; }
+    }
+}
