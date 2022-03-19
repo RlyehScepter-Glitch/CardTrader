121cbf81cab121967ffa4e996950728ed6cfbfac
RlyehScepter <rlyehscepter@gmail.com>
2022-03-19 15:35:18 +0200

    Service collection extension methods

---
+++ CardTrader/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using CardTrader.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+    }
+}
