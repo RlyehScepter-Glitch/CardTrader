f786056c3b62c76339c18cdd57f841ff52ce69e7
RlyehScepter <rlyehscepter@gmail.com>
2022-03-21 18:15:14 +0200

    Pages in Card and Home controllers now allow anonymous users

--- CardTrader/Controllers/HomeController.cs
+++ CardTrader/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CardTrader.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,11 +14,13 @@ namespace CardTrader.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return View();
