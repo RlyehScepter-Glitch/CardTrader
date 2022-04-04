a5caaa31493faac735a80340d81bf9f120529ca3
RlyehScepter <rlyehscepter@gmail.com>
2022-04-04 12:52:32 +0300

    Card controller updated

--- CardTrader/Controllers/CardController.cs
+++ CardTrader/Controllers/CardController.cs
@@ -1,5 +1,7 @@
 ﻿using CardTrader.Core.Contracts;
 using CardTrader.Core.DataTransferObjects;
+using CardTrader.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CardTrader.Controllers
@@ -7,9 +9,15 @@ namespace CardTrader.Controllers
     public class CardController : BaseController
     {
         private readonly ICardService cardService;
-        public CardController(ICardService _cardService)
+        private readonly IRepository repo;
+        private readonly UserManager<ApplicationUser> userManager;
+        public CardController(ICardService _cardService,
+            IRepository _repo,
+            UserManager<ApplicationUser> _userManager)
         {
             cardService = _cardService;
+            repo = _repo;
+            userManager = _userManager;
         }
         public IActionResult Create()
         {
@@ -29,5 +37,37 @@ namespace CardTrader.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(
+            string _Name,
+            string _Expansion,
+            string _Rarity,
+            string _Language,
+            bool _FirstEdition,
+            string _MinimumCondition,
+            string _ImageUrl,
+            string collectionName)
+        {
+            var user = await userManager.GetUserAsync(this.HttpContext.User);
+            Collection _Collection;
+
+            if (collectionName == "Binder")
+            {
+                _Collection = user.TradeBinder;
+            }
+            else
+            {
+                _Collection = user.WantedList;
+            }
+
+            Card card = cardService.CreateCard(_Name, _Expansion, _Rarity, _Language, _FirstEdition, _MinimumCondition, _ImageUrl, _Collection);
+
+            repo.Add(card);
+            repo.SaveChanges();
+
+            return Redirect("/Collection/Binder");
+        }
+
     }
 }
