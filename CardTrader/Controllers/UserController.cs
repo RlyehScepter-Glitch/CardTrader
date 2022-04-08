c8b62e6f98c8858c854bf5b70fa9dbe03a98b2ea
RlyehScepter <rlyehscepter@gmail.com>
2022-04-08 20:25:32 +0300

    User, UserBinder and UserWanted pages added to UserController

--- CardTrader/Controllers/UserController.cs
+++ CardTrader/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using CardTrader.Infrastructure.Data.Models;
+﻿using CardTrader.Core.Contracts;
+using CardTrader.Core.Services;
+using CardTrader.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,13 +9,47 @@ namespace CardTrader.Controllers
     public class UserController : BaseController
     {
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly IUserService userService;
+        private readonly ICollectionService collectionService;
 
-        public UserController(UserManager<ApplicationUser> _userManager)
+        public UserController(UserManager<ApplicationUser> _userManager,
+            IUserService _userService,
+            ICollectionService _collectionService)
         {
             userManager = _userManager;
+            userService = _userService;
+            collectionService = _collectionService;
         }
-        public IActionResult Account()
+        public IActionResult User(string username)
         {
+            var user = userService.GetUserByName(username);
+
+            ViewBag.Username = username;
+            
+            return View();
+        }
+
+        public IActionResult UserBinder(string username)
+        {
+            var user = userService.GetUserByName(username);
+            var binderId = user.BinderId;
+
+            var cards = collectionService.GetCards(binderId);
+
+            ViewData["Cards"] = cards;
+
+            return View();
+        }
+
+        public IActionResult UserWanted(string username)
+        {
+            var user = userService.GetUserByName(username);
+            var wantedListId = user.WantedListId;
+
+            var cards = collectionService.GetCards(wantedListId);
+
+            ViewData["Cards"] = cards;
+
             return View();
         }
     }
