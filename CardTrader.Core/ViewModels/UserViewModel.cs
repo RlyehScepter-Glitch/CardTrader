47d3cb6b3d88e2e2147d4b50af9e07c702d7b5c6
RlyehScepter <rlyehscepter@gmail.com>
2022-04-08 20:52:40 +0300

    UserViewModel created

---
+++ CardTrader.Core/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CardTrader.Core.ViewModels
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+        public string BinderId { get; set; }
+        public string WantedListId { get; set; }
+    }
+}
