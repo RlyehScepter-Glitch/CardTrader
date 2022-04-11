8a8c02b0dd28c233b2e103a9499e330679984512
RlyehScepter <rlyehscepter@gmail.com>
2022-04-11 23:25:52 +0300

    Index, Thread and Thread Creation implemented

--- CardTrader/Controllers/ForumController.cs
+++ CardTrader/Controllers/ForumController.cs
@@ -1,16 +1,79 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CardTrader.Core.Contracts;
+using CardTrader.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CardTrader.Controllers
 {
     public class ForumController : BaseController
     {
+        private readonly IForumService forumService;
+        private readonly IRepository repo;
+        private readonly IUserService userService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ForumController(IForumService _forumService,
+            IRepository _repo,
+            IUserService _userService,
+            UserManager<ApplicationUser> _userManager)
+        {
+            forumService = _forumService;
+            repo = _repo;
+            userService = _userService;
+            userManager = _userManager;
+        }
         public IActionResult Index()
         {
+            var threads = forumService.GetThreads();
+            ViewData["Threads"] = threads;
+
             return View();
         }
-        public IActionResult Thread()
+
+        [HttpPost]
+        [Route("/Forum/Thread")]
+        public IActionResult Thread(string threadId)
         {
+            var thread = forumService.GetThread(threadId);
+            var comments = forumService.GetComments(threadId);
+            var username = GetUser().Result.UserName;
+            
+            ViewData["Thread"] = thread;
+            ViewData["Comments"] = comments;
+            ViewData["Username"] = username;
+
             return View();
+        }
+
+        public IActionResult CreateThread()
+        {
+            var userId = GetUser()
+                .Result
+                .Id;
+
+            ViewData["UserId"] = userId;
+
+            return View();
+        }
+        
+        [HttpPost]
+        [Route("/Forum/Create")]
+        public IActionResult Create(string _userId,
+            string _title,
+            string _content)
+        {
+            var thread = forumService.CreateThread(_userId, _title, _content);
+
+            repo.Add(thread);
+            repo.SaveChanges();
+
+            return Redirect("/Forum/Index");
+        }
+
+        public async Task<ApplicationUser> GetUser()
+        {
+            var user = await userManager.GetUserAsync(this.HttpContext.User);
+            return user;
         }
     }
 }
