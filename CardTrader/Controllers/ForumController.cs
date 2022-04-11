bcd6443fb37c99596ffba37060b2f173c3c8d5a1
RlyehScepter <rlyehscepter@gmail.com>
2022-04-11 21:25:03 +0300

    Actions added to forum controller

--- CardTrader/Controllers/ForumController.cs
+++ CardTrader/Controllers/ForumController.cs
@@ -1,7 +1,16 @@
-﻿namespace CardTrader.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CardTrader.Controllers
 {
     public class ForumController : BaseController
     {
-
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Thread()
+        {
+            return View();
+        }
     }
 }
